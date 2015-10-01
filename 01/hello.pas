7188d1d66b9bce71ac4cf80daf98be25cac8295d
Sevkinapple <sevkinapple@gmail.com>
2015-10-01 21:30:32 +0300

    Update hello.pas

--- 01/hello.pas
+++ 01/hello.pas
@@ -1,11 +1,5 @@
 program HELLO;
 Begin
 	writeln('hello world');
-	writeln('Я крч человек');
-	writeln('Живу в Риге');
-	writeln('Подрабатываю с studio69 звукооператором');
-	writeln('учусь в Пушкинском лицее');
-	writeln('Там состаю в совете Лициистов');
-	writeln('И состаю в тех поддержке Лицея');
-	writeln('В свободно время подробатываю ремонотом ПК')
+	readln;
 end.	
